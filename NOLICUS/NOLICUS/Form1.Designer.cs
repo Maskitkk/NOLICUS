4673cfe91b6704c88688d9a20a8965ef59914dd2
Maskitkk <160743724+Maskitkk@users.noreply.github.com>
2024-03-27 14:38:15 +0300

    Add files via upload

---
+++ NOLICUS/NOLICUS/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace NOLICUS
+{
+    partial class NOLICUS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NOLICUS));
+            this.buttonLowLeft = new System.Windows.Forms.Button();
+            this.buttonMidLeft = new System.Windows.Forms.Button();
+            this.buttonUpLeft = new System.Windows.Forms.Button();
+            this.buttonLowCenter = new System.Windows.Forms.Button();
+            this.buttonMidCenter = new System.Windows.Forms.Button();
+            this.buttonUpCenter = new System.Windows.Forms.Button();
+            this.buttonLowRight = new System.Windows.Forms.Button();
+            this.buttonMidRight = new System.Windows.Forms.Button();
+            this.buttonUpRight = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonLowLeft
+            // 
+            resources.ApplyResources(this.buttonLowLeft, "buttonLowLeft");
+            this.buttonLowLeft.Name = "buttonLowLeft";
+            this.buttonLowLeft.UseVisualStyleBackColor = true;
+            this.buttonLowLeft.Click += new System.EventHandler(this.buttonLowLeft_Click);
+            // 
+            // buttonMidLeft
+            // 
+            resources.ApplyResources(this.buttonMidLeft, "buttonMidLeft");
+            this.buttonMidLeft.Name = "buttonMidLeft";
+            this.buttonMidLeft.UseVisualStyleBackColor = true;
+            this.buttonMidLeft.Click += new System.EventHandler(this.buttonMidLeft_Click);
+            // 
+            // buttonUpLeft
+            // 
+            resources.ApplyResources(this.buttonUpLeft, "buttonUpLeft");
+            this.buttonUpLeft.Name = "buttonUpLeft";
+            this.buttonUpLeft.UseVisualStyleBackColor = true;
+            this.buttonUpLeft.Click += new System.EventHandler(this.buttonUpLeft_Click);
+            // 
+            // buttonLowCenter
+            // 
+            resources.ApplyResources(this.buttonLowCenter, "buttonLowCenter");
+            this.buttonLowCenter.Name = "buttonLowCenter";
+            this.buttonLowCenter.UseVisualStyleBackColor = true;
+            this.buttonLowCenter.Click += new System.EventHandler(this.buttonLowCenter_Click);
+            // 
+            // buttonMidCenter
+            // 
+            resources.ApplyResources(this.buttonMidCenter, "buttonMidCenter");
+            this.buttonMidCenter.Name = "buttonMidCenter";
+            this.buttonMidCenter.UseVisualStyleBackColor = true;
+            this.buttonMidCenter.Click += new System.EventHandler(this.buttonMidCenter_Click);
+            // 
+            // buttonUpCenter
+            // 
+            resources.ApplyResources(this.buttonUpCenter, "buttonUpCenter");
+            this.buttonUpCenter.Name = "buttonUpCenter";
+            this.buttonUpCenter.UseVisualStyleBackColor = true;
+            this.buttonUpCenter.Click += new System.EventHandler(this.buttonUpCenter_Click);
+            // 
+            // buttonLowRight
+            // 
+            resources.ApplyResources(this.buttonLowRight, "buttonLowRight");
+            this.buttonLowRight.Name = "buttonLowRight";
+            this.buttonLowRight.UseVisualStyleBackColor = true;
+            this.buttonLowRight.Click += new System.EventHandler(this.buttonLowRight_Click);
+            // 
+            // buttonMidRight
+            // 
+            resources.ApplyResources(this.buttonMidRight, "buttonMidRight");
+            this.buttonMidRight.Name = "buttonMidRight";
+            this.buttonMidRight.UseVisualStyleBackColor = true;
+            this.buttonMidRight.Click += new System.EventHandler(this.buttonMidRight_Click);
+            // 
+            // buttonUpRight
+            // 
+            resources.ApplyResources(this.buttonUpRight, "buttonUpRight");
+            this.buttonUpRight.Name = "buttonUpRight";
+            this.buttonUpRight.UseVisualStyleBackColor = true;
+            this.buttonUpRight.Click += new System.EventHandler(this.buttonUpRight_Click);
+            // 
+            // NOLICUS
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.buttonUpRight);
+            this.Controls.Add(this.buttonMidRight);
+            this.Controls.Add(this.buttonUpCenter);
+            this.Controls.Add(this.buttonMidCenter);
+            this.Controls.Add(this.buttonLowRight);
+            this.Controls.Add(this.buttonUpLeft);
+            this.Controls.Add(this.buttonLowCenter);
+            this.Controls.Add(this.buttonMidLeft);
+            this.Controls.Add(this.buttonLowLeft);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Name = "NOLICUS";
+            this.Load += new System.EventHandler(this.NOLICUS_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLowLeft;
+        private System.Windows.Forms.Button buttonMidLeft;
+        private System.Windows.Forms.Button buttonUpLeft;
+        private System.Windows.Forms.Button buttonLowCenter;
+        private System.Windows.Forms.Button buttonMidCenter;
+        private System.Windows.Forms.Button buttonUpCenter;
+        private System.Windows.Forms.Button buttonLowRight;
+        private System.Windows.Forms.Button buttonMidRight;
+        private System.Windows.Forms.Button buttonUpRight;
+            
+    }
+
+
+}
+
---
+++ NOLICUS/NOLICUS/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+
+using System.Linq;
+
+using System.Windows.Forms;
+
+namespace NOLICUS
+{
+    public partial class NOLICUS : Form
+    {
+        //Задаём буул для определения был ли клик (чтобы чередовать символы)
+        
+        private bool buttonClicked = false;
+
+        public NOLICUS()
+        {
+            InitializeComponent();
+        }
+
+        private void NOLICUS_Load(object sender, EventArgs e)
+        {
+
+        }
+  
+
+
+
+
+        //Задаём вывод символа для клика по кнопке
+        private void buttonUpLeft_Click(object sender, EventArgs e)
+        {
+
+            if (buttonClicked)
+            {
+                buttonUpLeft.Text = "x";
+            }
+            else
+            {
+                buttonUpLeft.Text = "o";
+            }
+            buttonClicked = !buttonClicked;
+            WinCheck();
+        }
+
+        private void buttonUpCenter_Click(object sender, EventArgs e)
+        {
+            if (buttonClicked)
+            {
+                buttonUpCenter.Text = "x";
+            }
+            else
+            {
+                buttonUpCenter.Text = "o";
+            }
+            buttonClicked = !buttonClicked;
+            WinCheck();
+        }
+
+        private void buttonUpRight_Click(object sender, EventArgs e)
+        {
+            if (buttonClicked)
+            {
+                buttonUpRight.Text = "x";
+            }
+            else
+            {
+                buttonUpRight.Text = "o";
+            }
+            buttonClicked = !buttonClicked;
+            WinCheck();
+        }
+
+        private void buttonMidLeft_Click(object sender, EventArgs e)
+        {
+            if (buttonClicked)
+            {
+                buttonMidLeft.Text = "x";
+            }
+            else
+            {
+                buttonMidLeft.Text = "o";
+            }
+            buttonClicked = !buttonClicked;
+            WinCheck();
+        }
+
+        private void buttonMidCenter_Click(object sender, EventArgs e)
+        {
+            if (buttonClicked)
+            {
+                buttonMidCenter.Text = "x";
+            }
+            else
+            {
+                buttonMidCenter.Text = "o";
+            }
+            buttonClicked = !buttonClicked;
+            WinCheck();
+        }
+
+        private void buttonMidRight_Click(object sender, EventArgs e)
+        {
+            if (buttonClicked)
+            {
+                buttonMidRight.Text = "x";
+            }
+            else
+            {
+                buttonMidRight.Text = "o";
+            }
+            buttonClicked = !buttonClicked;
+            WinCheck();
+        }
+
+        private void buttonLowLeft_Click(object sender, EventArgs e)
+        {
+            if (buttonClicked)
+            {
+                buttonLowLeft.Text = "x";
+            }
+            else
+            {
+                buttonLowLeft.Text = "o";
+            }
+            buttonClicked = !buttonClicked;
+            WinCheck();
+        }
+
+        private void buttonLowCenter_Click(object sender, EventArgs e)
+        {
+            if (buttonClicked)
+            {
+                buttonLowCenter.Text = "x";
+            }
+            else
+            {
+                buttonLowCenter.Text = "o";
+            }
+            buttonClicked = !buttonClicked;
+            WinCheck();
+        }
+
+        private void buttonLowRight_Click(object sender, EventArgs e)
+        {
+            if (buttonClicked)
+            {
+                buttonLowRight.Text = "x";
+            }
+            else
+            {
+                buttonLowRight.Text = "o";
+            }
+            buttonClicked = !buttonClicked;
+            WinCheck();
+        }
+
+        //Проверка на победу или ничью
+
+        private bool WinCheck()
+        {
+            Button[] buttonsToCheck = { buttonUpLeft, buttonUpCenter, buttonUpRight, buttonMidLeft, buttonMidCenter, buttonMidRight, buttonLowLeft, buttonLowCenter, buttonLowRight };
+
+            //Проверка горизонталей
+            for (int i = 0; i < 3; i++)
+            {
+                int StartIndex = i * 3;
+                if (buttonsToCheck[StartIndex].Text != "" &&
+                    buttonsToCheck[StartIndex].Text == buttonsToCheck[StartIndex + 1].Text &&
+                    buttonsToCheck[StartIndex].Text == buttonsToCheck[StartIndex + 2].Text)
+                {
+                    MessageBox.Show(buttonsToCheck[StartIndex].Text + " победил");
+                    Reset();
+                    return true;
+                }
+            }
+
+            //Проверка вертикалей
+            for (int i = 0; i < 3; i++)
+            {
+                if (buttonsToCheck[i].Text != "" &&
+                        buttonsToCheck[i].Text == buttonsToCheck[i + 3].Text &&
+                        buttonsToCheck[i].Text == buttonsToCheck[i + 6].Text)
+                {
+                    MessageBox.Show(buttonsToCheck[i].Text + " победил");
+                    Reset();
+                    return true;
+
+                }
+            }
+
+            //Проверка диагоналей
+
+            if (buttonsToCheck[0].Text != "" &&
+                buttonsToCheck[0].Text == buttonsToCheck[4].Text &&
+                buttonsToCheck[0].Text == buttonsToCheck[8].Text)
+            {
+                MessageBox.Show(buttonsToCheck[0].Text + " победил");
+                Reset();
+                return true;
+            }
+            if (buttonsToCheck[2].Text != "" &&
+               buttonsToCheck[2].Text == buttonsToCheck[4].Text &&
+               buttonsToCheck[2].Text == buttonsToCheck[6].Text)
+            {
+                MessageBox.Show(buttonsToCheck[2].Text + " победил");
+                Reset();
+                return true;
+            }
+
+            //Проверка ничьей
+
+            bool allButtonsClicked = true;
+            foreach (Button button in buttonsToCheck)
+            {
+                if (button.Text == "")
+                {
+                    allButtonsClicked = false;
+                    break;
+                }
+            }
+            if (allButtonsClicked)
+            {
+
+                MessageBox.Show("Ничья");
+                Reset();
+                return true;
+            }
+
+            return false;
+
+        }
+
+        //Сброс игрового поля
+        private void Reset()
+        {
+            foreach (Button button in Controls.OfType<Button>())
+            {
+                button.Text = "";
+                button.Enabled = true;
+            }
+                buttonClicked = false;
+        }
+    
+
+    }
+
+ }
+           
+        
+
+
+  
+
